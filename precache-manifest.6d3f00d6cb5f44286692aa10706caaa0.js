ab69286fef9b889997a3504b88b6680c52c44c87
azran-azwer <azranmam@gmail.com>
2019-03-09 17:49:05 +0530

    Updates

---
+++ precache-manifest.6d3f00d6cb5f44286692aa10706caaa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "f3e345700426ee82c600",
+    "url": "/contactmanager/static/js/main.f3e34570.chunk.js"
+  },
+  {
+    "revision": "0a3ed132e73d563c13c8",
+    "url": "/contactmanager/static/js/2.0a3ed132.chunk.js"
+  },
+  {
+    "revision": "f3e345700426ee82c600",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "0a3ed132e73d563c13c8",
+    "url": "/contactmanager/static/css/2.4bf7a95b.chunk.css"
+  },
+  {
+    "revision": "ad34cf93130658249b81137d8a9e1fca",
+    "url": "/contactmanager/index.html"
+  }
+];
